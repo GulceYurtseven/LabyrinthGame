9abcdb408884d59b980dd0f3fc54b83ee6b49053
Beşan Gülce Yurtseven <132767560+GulceYurtseven@users.noreply.github.com>
2025-01-12 18:00:35 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿
+namespace lebirent
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.oyunaBaslabutton = new System.Windows.Forms.Button();
+            this.Sayılar = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.Harita = new System.Windows.Forms.ListBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.maviToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pembeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yeşilToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.kalanSureLabel = new System.Windows.Forms.Label();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton1.ForeColor = System.Drawing.Color.DarkRed;
+            this.radioButton1.Location = new System.Drawing.Point(13, 287);
+            this.radioButton1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(68, 24);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "kolay";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton2.ForeColor = System.Drawing.Color.DarkRed;
+            this.radioButton2.Location = new System.Drawing.Point(13, 324);
+            this.radioButton2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(59, 24);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "orta";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioButton3.ForeColor = System.Drawing.Color.DarkRed;
+            this.radioButton3.Location = new System.Drawing.Point(13, 362);
+            this.radioButton3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(54, 24);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "zor";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // oyunaBaslabutton
+            // 
+            this.oyunaBaslabutton.BackColor = System.Drawing.Color.Coral;
+            this.oyunaBaslabutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.oyunaBaslabutton.ForeColor = System.Drawing.Color.Maroon;
+            this.oyunaBaslabutton.Location = new System.Drawing.Point(89, 325);
+            this.oyunaBaslabutton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.oyunaBaslabutton.Name = "oyunaBaslabutton";
+            this.oyunaBaslabutton.Size = new System.Drawing.Size(128, 56);
+            this.oyunaBaslabutton.TabIndex = 4;
+            this.oyunaBaslabutton.Text = "oyuna başla";
+            this.oyunaBaslabutton.UseVisualStyleBackColor = false;
+            this.oyunaBaslabutton.Click += new System.EventHandler(this.oyunaBaslabutton_Click);
+            // 
+            // Sayılar
+            // 
+            this.Sayılar.BackColor = System.Drawing.SystemColors.Info;
+            this.Sayılar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Sayılar.FormattingEnabled = true;
+            this.Sayılar.ItemHeight = 20;
+            this.Sayılar.Location = new System.Drawing.Point(13, 39);
+            this.Sayılar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Sayılar.Name = "Sayılar";
+            this.Sayılar.Size = new System.Drawing.Size(218, 224);
+            this.Sayılar.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.OrangeRed;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(248, 275);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 49);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.OrangeRed;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(366, 275);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 49);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.OrangeRed;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(248, 336);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 45);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.OrangeRed;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(366, 336);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(99, 45);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Harita
+            // 
+            this.Harita.BackColor = System.Drawing.SystemColors.Info;
+            this.Harita.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Harita.FormattingEnabled = true;
+            this.Harita.ItemHeight = 20;
+            this.Harita.Location = new System.Drawing.Point(247, 39);
+            this.Harita.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Harita.Name = "Harita";
+            this.Harita.Size = new System.Drawing.Size(218, 224);
+            this.Harita.TabIndex = 12;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(492, 27);
+            this.toolStrip1.TabIndex = 13;
+            this.toolStrip1.Text = "toolStrip1";
+            this.toolStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.toolStrip1_ItemClicked);
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.maviToolStripMenuItem,
+            this.pembeToolStripMenuItem,
+            this.yeşilToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(68, 24);
+            this.toolStripDropDownButton1.Text = "renkler";
+            // 
+            // maviToolStripMenuItem
+            // 
+            this.maviToolStripMenuItem.Name = "maviToolStripMenuItem";
+            this.maviToolStripMenuItem.Size = new System.Drawing.Size(139, 26);
+            this.maviToolStripMenuItem.Text = "mavi";
+            this.maviToolStripMenuItem.Click += new System.EventHandler(this.maviToolStripMenuItem_Click);
+            // 
+            // pembeToolStripMenuItem
+            // 
+            this.pembeToolStripMenuItem.Name = "pembeToolStripMenuItem";
+            this.pembeToolStripMenuItem.Size = new System.Drawing.Size(139, 26);
+            this.pembeToolStripMenuItem.Text = "pembe";
+            this.pembeToolStripMenuItem.Click += new System.EventHandler(this.pembeToolStripMenuItem_Click);
+            // 
+            // yeşilToolStripMenuItem
+            // 
+            this.yeşilToolStripMenuItem.Name = "yeşilToolStripMenuItem";
+            this.yeşilToolStripMenuItem.Size = new System.Drawing.Size(139, 26);
+            this.yeşilToolStripMenuItem.Text = "yeşil";
+            this.yeşilToolStripMenuItem.Click += new System.EventHandler(this.yeşilToolStripMenuItem_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(92, 289);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "kalan süre:";
+            // 
+            // kalanSureLabel
+            // 
+            this.kalanSureLabel.AutoSize = true;
+            this.kalanSureLabel.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.kalanSureLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.kalanSureLabel.Location = new System.Drawing.Point(194, 288);
+            this.kalanSureLabel.Name = "kalanSureLabel";
+            this.kalanSureLabel.Size = new System.Drawing.Size(21, 20);
+            this.kalanSureLabel.TabIndex = 15;
+            this.kalanSureLabel.Text = "--";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 406);
+            this.Controls.Add(this.kalanSureLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.Harita);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Sayılar);
+            this.Controls.Add(this.oyunaBaslabutton);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button oyunaBaslabutton;
+        private System.Windows.Forms.ListBox Sayılar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox Harita;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem maviToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pembeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yeşilToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label kalanSureLabel;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace lebirent
+{
+    public partial class Form1 : Form
+    {     
+        public Form1()
+        {
+            InitializeComponent();
+            radioButton1.Enabled = true;
+            radioButton2.Enabled = true;
+            radioButton3.Enabled = true;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            timer1.Enabled = false;
+        }   
+
+        string[,] oyun = new string[10, 4];
+        int[] sayi = new int[10];
+        int j, kolaySaniye = 50, ortaSaniye = 25, zorSaniye = 10;
+
+        private void buttonAktiflestir()
+        {
+            radioButton1.Enabled = true;
+            radioButton2.Enabled = true;
+            radioButton3.Enabled = true;
+            timer1.Enabled = true;
+        }
+        
+        private void xler()
+        {
+            string yedek = "";
+            //string[,] oyun = new string[10, 4];
+            for (int i = 0; i < 10; i++)
+            {
+                yedek = "";
+                for (int j = 0; j < 4; j++)
+                {
+                    yedek = yedek  + "  " + oyun[i, j] ;// x leri yerleştir
+                }
+                Harita.Items.Add(yedek);
+            }
+        }
+
+        private void oyunaBaslabutton_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    oyun[i, j] = "X";// x leri diziye al
+                }
+            }           
+            for (int i = 0; i < 10; i++)
+            {
+                sayi[i] = rand.Next(1, 5);
+                Sayılar.Items.Add(sayi[i].ToString());
+            }
+            xler();
+            buttonAktiflestir();
+        }
+
+        int a = 0, b = 0, k = 5;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            j = Convert.ToInt16(button1.Text);
+            if (sayi[a] == j){
+                oyun[b, j-1] = "*";
+                b++;
+                a++;
+                if (b == 10)
+                {
+                    MessageBox.Show("TEBRİKLER!!");
+                }
+            }
+            else
+            {
+                k--;
+                MessageBox.Show("HATA YAPTIN kalan hakkın: "+ k.ToString());
+                if (k == 0)
+                {
+                    //MessageBox.Show("AĞLA");
+                    button1.Enabled = false;
+                    button2.Enabled = false;
+                    button3.Enabled = false;
+                    button4.Enabled = false;
+                    oyunaBaslabutton.Enabled = false;
+
+                }
+            }
+            Harita.Items.Clear();
+            xler();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            j = Convert.ToInt16(button2.Text);
+            if (sayi[a] == j)
+            {
+                oyun[b, j - 1] = "*";
+                b++;
+                a++;
+                if (b == 10)
+                {
+                    MessageBox.Show("TEBRİKLER!!");
+                }
+            }
+            else
+            {
+                k--;
+                MessageBox.Show("HATA YAPTIN kalan hakkın: "+ k.ToString());
+                if (k == 0)
+                {
+                   // MessageBox.Show("AĞLA");
+                    button1.Enabled = false;
+                    button2.Enabled = false;
+                    button3.Enabled = false;
+                    button4.Enabled = false;
+                    oyunaBaslabutton.Enabled = false;
+
+                }
+            }
+            Harita.Items.Clear();
+            xler();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            j = Convert.ToInt16(button3.Text);
+            if (sayi[a] == j)
+            {
+                oyun[b, j - 1] = "*";
+                b++;
+                a++;
+                if (b == 10)
+                {
+                    MessageBox.Show("TEBRİKLER!!");
+                }
+            }
+            else
+            {
+                k--;
+                MessageBox.Show("HATA YAPTIN kalan hakkın: "+ k.ToString());
+                if (k == 0)
+                {
+                    //MessageBox.Show("AĞLA");
+                    button1.Enabled = false;
+                    button2.Enabled = false;
+                    button3.Enabled = false;
+                    button4.Enabled = false;
+                    oyunaBaslabutton.Enabled = false;
+
+                }
+            }
+            Harita.Items.Clear();
+            xler();
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            j = Convert.ToInt16(button4.Text);
+            if (sayi[a] == j)
+            {
+                oyun[b, j - 1] = "*";
+                b++;
+                a++;
+                if (b == 10)//sona ulaşmış demektir
+                {
+                    MessageBox.Show("TEBRİKLER!!");
+                }
+            }
+            else
+            {
+                k--;
+                MessageBox.Show("HATA YAPTIN kalan hakkın: "+ k.ToString());
+                if (k == 0)
+                {
+                    //MessageBox.Show("AĞLA");
+                    button1.Enabled = false;
+                    button2.Enabled = false;
+                    button3.Enabled = false;
+                    button4.Enabled = false;
+                    oyunaBaslabutton.Enabled = false;    
+                }
+            }
+            Harita.Items.Clear();
+            xler();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                kolaySaniye--;
+                kalanSureLabel.Text = kolaySaniye.ToString();
+                
+                if (kolaySaniye == 0)
+                {
+                    timer1.Stop();
+                    Sayılar.Visible = false;
+                    button1.Enabled = true;
+                    button2.Enabled = true;
+                    button3.Enabled = true;
+                    button4.Enabled = true;
+                }
+            }
+            if (radioButton2.Checked)
+            {
+               
+                ortaSaniye--;
+                kalanSureLabel.Text = ortaSaniye.ToString();
+                if (ortaSaniye == 0)
+                {
+                    timer1.Stop();
+                    Sayılar.Visible = false;
+                    button1.Enabled = true;
+                    button2.Enabled = true;
+                    button3.Enabled = true;
+                    button4.Enabled = true;
+                }
+            }
+            if (radioButton3.Checked)
+            {
+              
+                zorSaniye--;
+                kalanSureLabel.Text = zorSaniye.ToString();
+                if (zorSaniye == 0)
+                {
+                    timer1.Stop();
+                    Sayılar.Visible = false;
+                    button1.Enabled = true;
+                    button2.Enabled = true;
+                    button3.Enabled = true;
+                    button4.Enabled = true;
+                }
+            }
+        }
+
+        private void maviToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            BackColor = Color.DarkSlateBlue;
+        }
+      
+        private void pembeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            BackColor = Color.Pink;
+        }
+
+        private void yeşilToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            BackColor = Color.LightGreen;
+        }
+    }
+}
